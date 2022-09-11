27aa8c3782ba9fbf869981eed330bac42c8cf593
Ye Weibin <yeweibin@gmail.com>
2022-09-11 20:36:36 +0800

    增加NVOD业务相关描述符解析

---
+++ src/main/java/m2tk/dvb/decoder/descriptor/TimeShiftedServiceDescriptorDecoder.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Ye Weibin. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package m2tk.dvb.decoder.descriptor;
+
+import m2tk.encoding.Encoding;
+import m2tk.mpeg2.decoder.DescriptorDecoder;
+
+public class TimeShiftedServiceDescriptorDecoder extends DescriptorDecoder
+{
+    public static final int TAG = 0x4C;
+
+    public TimeShiftedServiceDescriptorDecoder()
+    {
+        super("TimeShiftedServiceDescriptorDecoder");
+    }
+
+    @Override
+    public boolean isAttachable(Encoding target)
+    {
+        return super.isAttachable(target) && target.readUINT8(0) == TAG && target.size() >= 4;
+    }
+
+    public int getReferenceServiceID()
+    {
+        return encoding.readUINT16(2);
+    }
+}
